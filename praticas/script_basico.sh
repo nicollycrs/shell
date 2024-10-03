42485358212463679744c827dd05ea863c9a54f5
Nicolly <80932261+nicollycrs@users.noreply.github.com>
2024-10-03 00:35:55 -0300

    Update script_basico.sh

--- praticas/script_basico.sh
+++ praticas/script_basico.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+# Primeiro script bash
 
+echo "Olá, mundo!"
+echo "Seu nome é: $1" #Passando parâmetros
+echo "Sua idade é $2" 
+echo "Número de parâmetros passados ao script: $#"
+echo "Nome do script: $0"
+echo "Lista de todos os parâmetros passados: $@"
+echo "Todos os parâmetros em forma de string: $*"
+echo "Deu certo executar o comando anterior? 0 - sim; 1 - não. Resultado: $?"
